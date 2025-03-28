7e730e8b4864e3e1440899f8ab917f5b8e23c770
Emmanuel Nortey-King <nkingemma@.gmail.com>
2025-03-28 09:24:23 +0000

    integration for dashboard

--- src/pages/dashboard/SingleVendorAdvert.jsx
+++ src/pages/dashboard/SingleVendorAdvert.jsx
@@ -1,17 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import { apiGetSingleAdvert } from '../../services/adverts';
-import Sidebar from '../../components/Sidebar';
 
 const SingleVendorAdvert = () => {
 
-
-    
   return (
-    <>
-    <Sidebar/>
     <div>SingleVendorAdvert</div>
-    </>
   )
 }
 
